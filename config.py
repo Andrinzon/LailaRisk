593f43a7ffe09561083908f55f8aded5a6c65931
Andrinzon <133286950+Andrinzon@users.noreply.github.com>
2025-04-28 10:53:09 -0400

    Delete config.py

--- config.py
+++
@@ -1,3 +0,0 @@
-DISCORD_TOKEN = 'MTA5MjYyNzQ3NzQ3NTY0MzQyMg.Gv8ly4.pJxdRD2QDJgiGQ9NSAr1Y6M5PwT2yLKv3YNAks'
-CANAL_CONTROL_ID = 1366420217739083828  # ID del canal donde controlarás el bot
-ADMIN_IDS = [637107444979335179]       # IDs de admins que pueden cambiar riesgos
